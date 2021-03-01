20ed0c0fc3d7d0cba2d2f5b02816ffc26d5ff1d5
Yuri Ranieri <yurifranieri@gmail.com>
2021-03-01 17:32:09 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/yuriranieri/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.yuriranieri.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yuriranieri.cursomc.domain.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
